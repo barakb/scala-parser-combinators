ca3552a109bbb3c636a4129d8018d82d4ca380d6
Barak Bar Orion <barak.bar@gmail.com>
2017-09-24 08:45:24 +0300

    Initial Commit.

---
+++ src/main/scala/com/gigaspaces/Main.scala
@@ -0,0 +1,10 @@
+package com.gigaspaces
+
+/**
+  * Created by Barak Bar Orion
+  * on 9/18/17.
+  */
+object Main extends App{
+
+
+}
---
+++ src/main/scala/com/gigaspaces/Parsers.scala
@@ -0,0 +1,19 @@
+package com.gigaspaces
+
+
+/**
+  * Created by Barak Bar Orion
+  * on 9/18/17.
+  */
+
+
+object Parsers{
+
+  //  run(char(c))(c.toString) == Right(c)
+//  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+  //  run(char(c))(c.toString) == Right(c)
+//  def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
+
+}
+
