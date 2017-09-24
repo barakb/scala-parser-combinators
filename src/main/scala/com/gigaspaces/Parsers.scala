74aceda69668d8c931cc914bf6024043deee0e2f
Barak Bar Orion <barak.bar@gmail.com>
2017-09-24 09:48:40 +0300

    Update README

--- src/main/scala/com/gigaspaces/Parsers.scala
+++ src/main/scala/com/gigaspaces/Parsers.scala
@@ -9,11 +9,5 @@ package com.gigaspaces
 
 object Parsers{
 
-  //  run(char(c))(c.toString) == Right(c)
-//  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
-
-  //  run(char(c))(c.toString) == Right(c)
-//  def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
-
 }
 
